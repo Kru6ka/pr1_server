4a7b02b0ce32f8edf44d142d83c0f08b7473c52f
mclvren <aiv-n@ya.ru>
2019-07-04 20:43:57 +0300

    Format

--- server.c
+++ server.c
@@ -82,45 +82,45 @@ int main() {
   int sockfd, connfd, len;
   struct sockaddr_in servaddr, cli;
 
-  // socket create and verification
+  // Создание и верификация сокета
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
-    printf("socket creation failed...\n");
+    printf("Socket creation failed...\n");
     exit(0);
   } else
     printf("Socket successfully created..\n");
   bzero(&servaddr, sizeof(servaddr));
 
-  // assign IP, PORT
+  // Назначение IP, PORT
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(PORT);
 
-  // Binding newly created socket to given IP and verification
+  // Binding сокета к IP и верификация
   if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0) {
-    printf("socket bind failed...\n");
+    printf("Socket bind failed...\n");
     system(RED_ON);
-    sleep(3);
+    sleep(30);
     system(RED_OFF);
     exit(0);
   } else
     printf("Socket successfully binded..\n");
 
-  // Now server is ready to listen and verification
+  // Теперь сервер готов прослушивать порт
   if ((listen(sockfd, 5)) != 0) {
     printf("Listen failed...\n");
     exit(0);
   } else
-    printf("Server listening..\n");
+    printf("Server listening on port: %i..\n",PORT);
   len = sizeof(cli);
 
-  // Accept the data packet from client and verification
+  // Принятие подключение клиента
   connfd = accept(sockfd, (SA *)&cli, &len);
   if (connfd < 0) {
-    printf("server acccept failed...\n");
+    printf("Server acccept failed...\n");
     exit(0);
   } else {
-    printf("server acccept the client...\n");
+    printf("Server acccept the client...\n");
     system(GRN_ON);
   }
 
@@ -129,6 +129,7 @@ int main() {
 
   // Закрытие сокета
   close(sockfd);
+	// Выключение диодов
   system(BLU_OFF);
   system(GRN_OFF);
   system(RED_OFF);
